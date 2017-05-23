debe4b75a98fb8801a2c595fe1473acc952cd6c0
Lars Funke <lars.funke@udo.edu>
2017-05-23 14:32:18 +0200

    auswertung und disk 16

--- 16/auswertung/dicke.py
+++ 16/auswertung/dicke.py
@@ -4,38 +4,61 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
 import scipy.constants as const
+import tools
+import uncertainties as unc
+import uncertainties.unumpy as unp
 
+data1 = np.genfromtxt('daten/dicke_ohne.txt', unpack=True, dtype=object)
+data2 = np.genfromtxt('daten/dicke_mit.txt', unpack=True, dtype=object)
 
-U1, p1 = np.genfromtxt('daten/dicke_ohne.txt', unpack=True)
-U2, p2 = np.genfromtxt('daten/dicke_mit.txt', unpack=True)
+tools.table([data1[0], data1[1], data2[0], data2[1]], ["U_1/V", "p_1/hPa", "U_2/V", "p_2/hPa"], "build/table_energieverlust.tex", "Messdaten der Energieverlustmessung", "tab:daten1", split=1, footer=None, round_figures=None, interrows=None)
+
+U1, p1 = data1.astype(float)
+U2, p2 = data2.astype(float)
+
 
 slope1, intercept1, r_value, p_value, std_err1 = stats.linregress(p1, U1)
 slope2, intercept2, r_value, p_value, std_err2 = stats.linregress(p2, U2)
 
+mx = p1.mean()
+sx2 = ((p1-mx)**2).sum()
+intercept_err1 = std_err1 * np.sqrt(1./len(p1) + mx*mx/sx2)
+
+mx = p2.mean()
+sx2 = ((p2-mx)**2).sum()
+intercept_err2 = std_err2 * np.sqrt(1./len(p2) + mx*mx/sx2)
+
+intercept1 = unc.ufloat(intercept1, intercept_err1)
+intercept2 = unc.ufloat(intercept2, intercept_err2)
+
+
 x = np.linspace(0, 325)
 
-plt.plot(x, slope1 * x + intercept1)
-plt.plot(x, slope2 * x + intercept2)
+plt.plot(x, slope1 * x + intercept1.n, 'C0', label='Fit ohne Folie')
+plt.plot(x, slope2 * x + intercept2.n, 'C1', label='Fit mit Folie')
 
-print('Pulshöhendifferenz: {}'.format(intercept2-intercept1))
-print('Druckdifferenz: {}'.format(-intercept2/slope2 + intercept1/slope1))
-plt.plot(p1, U1, 'x')
-plt.plot(p2, U2, 'x')
+print('Pulshöhendifferenz: {}'.format(intercept1-intercept2))
+print('Druckdifferenz: {}'.format(-intercept1/slope1 + intercept2/slope2))
+plt.plot(p1, U1, 'x', label='Daten ohne Folie')
+plt.plot(p2, U2, 'x', label='Daten mit Folie')
 
 plt.xlabel(r'$p/\mathrm{hPa}$')
 plt.ylabel(r'$U/\mathrm{V}$')
 plt.xlim(0, 325)
 plt.ylim(0, 9)
+plt.legend(loc='best')
 plt.tight_layout(pad=0)
 plt.savefig('build/plot2.pdf')
 
 z = 2
-p = (-intercept2/slope2 + intercept1/slope1) * 100
+p = (+intercept2/slope2 - intercept1/slope1) * 100
 T = 295
 N = p / (const.k * T)
 Z = 7
 m = const.m_e
 Ekin = 5.486e6 * const.e
+#Ekin = 5.16e6 * const.e
+
 E0 = 3727e6 * const.e
 v = const.c * np.sqrt(1-(1/(1+Ekin/E0))**2)
 i = 10 * const.e * Z
@@ -46,10 +69,11 @@ de = 4 * np.pi * const.e**4 * z**2 * N * Z / (m * v**2 * (4 * np.pi * const.epsi
 Z = 79
 N = 97986 * const.N_A
 i = 10 * const.e * Z
+print(N)
 
-dx = de / (4 * np.pi * const.e**4 * z**2 * N * Z / (m * v**2 * (4 * np.pi * const.epsilon_0)**2) * np.log(2 * m * v**2 / i))
-print(dx)
+#dx = de / (4 * np.pi * const.e**4 * z**2 * N * Z / (m * v**2 * (4 * np.pi * const.epsilon_0)**2) * np.log(2 * m * v**2 / i))
+#print('{}µm'.format(dx*1e6))
 
-de = 5.486e6 * const.e / intercept1 * (intercept2-intercept1)
+de = 5.486e6 * const.e * (1-intercept2/intercept1)
 dx = de / (4 * np.pi * const.e**4 * z**2 * N * Z / (m * v**2 * (4 * np.pi * const.epsilon_0)**2) * np.log(2 * m * v**2 / i))
-print(dx)
+print('{}µm'.format(dx*1e6))
--- 16/auswertung/schönerplot.py
+++ 16/auswertung/schönerplot.py
@@ -3,20 +3,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.constants as const
+import tools
+import uncertainties.unumpy as unp
 
-theta, N, t = np.genfromtxt('daten/streuquerschnitt.txt', unpack=True)
+data = np.genfromtxt('daten/streuquerschnitt.txt', unpack=True, dtype=object)
 
-
+theta, N, t = data.astype(float)
 
 N0 = 5360/180
 N = N/t
+N = unp.uarray(N, np.sqrt(N))
 F = 2e-3 * 10e-3
 N_T = 2e-6 * F * 97986 * const.N_A
 
 sigma = N/N0 * F/N_T
-difsigma = sigma / (F / 101e-3**2)
+omega = (F / 101e-3**2)
+print(omega)
+difsigma = sigma / omega
 
-plt.plot(theta, difsigma, 'x')
+tools.table([data[0], data[1], data[2], N, difsigma], [r'\theta/\degree', r'N_\text{mess}', 't/s', r'N/\per\second', r'\frac{\mathrm{d}\sigma}{\mathrm{d}\Omega}(\theta)/\mathrm{m}^2'], 'build/daten2.tex', 'Messwerte und daraus abgeleitete Größen bei der Messung des Streuquerschnitts.', 'tab:daten2', split=1)
+
+plt.errorbar(theta, unp.nominal_values(difsigma), yerr=unp.std_devs(difsigma), fmt='x', label='Messwerte')
 plt.xlabel(r'$\theta/^\circ$')
 plt.ylabel(r'$\frac{\mathrm{d}\sigma}{\mathrm{d}\Omega}(\theta)/\mathrm{m}^2$')
 plt.ylim(-0.1e-21, 2.6e-21)
@@ -24,6 +31,7 @@ plt.xlim(-5, 25)
 
 theta_th = np.linspace(1.5, 27, 1000)
 theosigma = 1/(4 * np.pi * const.epsilon_0) ** 2 * (2 * 79 * const.e ** 2 / (4 * 5.486e6 * const.e))**2 * 1 / (np.sin(theta_th / 360 * 2 * np.pi/2) ** 4)
-plt.plot(theta_th, theosigma, '-')
+plt.plot(theta_th, theosigma, '-', label='Theoriekurve')
+plt.legend(loc='best')
 plt.tight_layout(pad=0)
 plt.savefig('build/plot3.pdf')
