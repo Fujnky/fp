c3d50497ed83e803acb22961a8353a3ec808e6a8
Lars Funke <lars.funke@udo.edu>
2017-06-24 14:03:39 +0200

    Auswertung

--- 21/auswertung/g.py
+++ 21/auswertung/g.py
@@ -1,24 +1,65 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.constants as const
+import scipy.stats
+import uncertainties as unc
+import uncertainties.unumpy as unp
+import tools
 
-f, I11, I12, I21, I22 = np.genfromtxt('daten/daten.txt', unpack=True)
+data = np.genfromtxt('daten/daten.txt', dtype=object, unpack=True)
+f, I11, I12, I21, I22 = data.astype(float)
 
-f*=1e6
+f *= 1e6
 
 def helmholtz(I, N, r):
     return 8/np.sqrt(125) * const.mu_0 * N * I / r
 
-B1 = helmholtz(I11, 11, 16.39e-2) + helmholtz(I12, 154, 15.79e-2)
-B2 = helmholtz(I21, 11, 16.39e-2) + helmholtz(I22, 154, 15.79e-2)
 
-g1 = const.h * f / (B1 * const.value('Bohr magneton'))
-g2 = const.h * f / (B2 * const.value('Bohr magneton'))
+def auswerten(I1, I2, i, Z):
 
-print(const.value('Bohr magneton'))
-print(g1.mean())
-print(g2.mean())
+    B = helmholtz(I1, 11, 16.39e-2) + helmholtz(I2, 154, 15.79e-2)
+    plt.plot(f/1e6, B*1e6, 'C{}x'.format(i),
+             label=r'Messdaten von $^{'+Z+r'}\mathrm{Rb}$')
 
-plt.plot(f/1e6, B1*1e6, 'x')
-plt.plot(f/1e6, B2*1e6, 'x')
-plt.show()
+    x = f
+    y = B
+    slope, intercept, r, prob2, see = scipy.stats.linregress(x, y)
+    mx = x.mean()
+    sx2 = ((x-mx)**2).sum()
+    sd_intercept = see * np.sqrt(1./len(x) + mx*mx/sx2)
+    sd_slope = see * np.sqrt(1./sx2)
+    f_syn = np.linspace(0, 1.1)
+    plt.plot(f_syn, 1e6*(slope*f_syn*1e6+intercept),
+             label='Lineare Regression')
+    slope = unc.ufloat(slope, sd_slope)
+    intercept = unc.ufloat(intercept, sd_intercept)
+    # print('slope: {}µT/MHz, intercept: {} µT'.format(
+    #    slope*1e6, intercept*1e6))
+
+    g = 1/(slope / const.h * const.value('Bohr magneton'))
+    print('g-Faktor Rb-{}: {}'.format(Z, g))
+
+    return g, intercept, B
+
+
+g1, emf1, B1 = auswerten(I11, I12, 0, '87')
+g2, emf2, B2 = auswerten(I21, I22, 1, '85')
+
+print((I11, I12, B1*1e6, I21, I22, B2*1e6))
+tools.table((*data[:3], B1*1e6, *data[3:], B2*1e6), ('f/MHz', 'I_1^\mathrm{sweep}/A', 'I_1^\mathrm{hor}/A', r'B_1/\micro\tesla', 'I_2^\mathrm{sweep}/A', 'I_2^\mathrm{hor}/A', r'B_2/\micro\tesla'), 'build/feld.tex', 'Bestimmte Stromstärken der jeweiligen Resonanzstellen und daraus berechnete magnetische Flussdichten.', 'tab:feld')
+
+emf = unc.ufloat(np.mean((emf1.n, emf2.n)), np.std((emf1.n, emf2.n)))
+
+print('Gemitteltes Horizontalkomponente des Erdmagnetfelds: {}µT'.format(
+                                                                1e6*emf))
+
+print('Kompensiertes Vertikalfeld: {}µT'.format(helmholtz(0.23, 20, 11.735e-2)*1e6))
+plt.xlabel(r'$f/\mathrm{MHz}$')
+plt.ylabel(r'$B/\mathrm{µT}$')
+plt.xlim(0, 1.1)
+t = list(plt.yticks()[0])
+plt.yticks(t + [emf.n*1e6], ['{0:8.0f}'.format(ti) for ti in t]+[r'$B_\mathrm{hor}^\oplus$'])
+
+plt.legend(loc='best')
+plt.tight_layout(pad=0)
+plt.savefig('build/plot1.pdf')
--- 21/auswertung/plot.py
+++ 21/auswertung/plot.py
@@ -1,18 +1,57 @@
 import matplotlib.pyplot as plt
-import  numpy as np
+import numpy as np
+import scipy.optimize
 
-a,b,c,t1,I1,z = np.genfromtxt('daten/I1.csv', unpack=True, delimiter=',')
-a,b,c,t2,I2,z = np.genfromtxt('daten/I2.csv', unpack=True, delimiter=',')
-a,b,c,t3,U3,z = np.genfromtxt('daten/U1.csv', unpack=True, delimiter=',')
-a,b,c,t4,U4,z = np.genfromtxt('daten/U2.csv', unpack=True, delimiter=',')
-a,b,c,t5,U5,z = np.genfromtxt('daten/U3.csv', unpack=True, delimiter=',')
+a, b, c, t1, I1, z = np.genfromtxt('daten/I1.csv', unpack=True, delimiter=',')
+a, b, c, t2, I2, z = np.genfromtxt('daten/I2.csv', unpack=True, delimiter=',')
+a, b, c, t3, U3, z = np.genfromtxt('daten/U1.csv', unpack=True, delimiter=',')
+a, b, c, t4, U4, z = np.genfromtxt('daten/U2.csv', unpack=True, delimiter=',')
+a, b, c, t5, U5, z = np.genfromtxt('daten/U3.csv', unpack=True, delimiter=',')
 
-#plt.plot(t1, I1, '.')
-t = (t2+1.1)/2
 
-plt.xlim(0, 1)
-lims = [[650, 800], [1180, 1290], [1400, 1600]]
-plt.plot(t, I2/100, 'x', markersize=2)
-plt.plot(t[lims[2][0]:lims[2][1]], U5[lims[2][0]:lims[2][1]], 'x', markersize=2)
+def cauchy(x, N, s, t, b):
+    return -N/np.pi * (s/(s**2 + (x-t)**2))+b
 
-plt.show()
+
+t = (t2+1.1)
+
+lims = [[650+20, 800-30], [1180-30, 1290+40], [1430-30, 1550+30]]
+
+
+
+ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5)
+plt.plot(t, U5, 'x', markersize=2, label='Messdaten')
+plt.setp(ax1.get_xticklabels(), visible=False)
+plt.ylabel('Intensität/a.u.')
+
+
+plateau = U5[1700:1800].mean()
+print('Plateau: {}'.format(plateau))
+
+print('Minima:')
+i = 1
+for lim in lims:
+    t_sl = t[lim[0]:lim[1]]
+    U_sl = U5[lim[0]:lim[1]]
+    params, pcov = scipy.optimize.curve_fit(cauchy, t_sl, U_sl, maxfev=1000000)
+
+    t_syn = np.linspace(t[lim[0]], t[lim[1]], 1000)
+    plt.plot(t_syn, cauchy(t_syn, *params), label='Fit {}'.format(i))
+
+    print(params[2], plateau-cauchy(params[2], *params))
+    i+=1
+
+
+plt.legend(loc='best')
+
+# share x only
+ax2 = plt.subplot2grid((6, 1), (5, 0), sharex=ax1)
+# make these tick labels invisible
+plt.ylabel(r'$I/\mathrm{A}$')
+plt.plot(t, (I2+142)/278  , 'x', markersize=1)
+plt.xlabel(r'$t/\mathrm{s}$')
+plt.setp(ax2.get_xticklabels())
+plt.xlim(-0.05, 2.05)
+
+plt.tight_layout(pad=0)
+plt.savefig('build/plot2.pdf')
